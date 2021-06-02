7a9e719619001f15e20fb19b67868eab5ae15828
Jogvan Patursson <jogvan.pat18@student.setur.fo>
2021-06-02 13:37:25 +0100

    Update ddl.sql

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -11,79 +11,79 @@
 --Create Person table--
 DROP TABLE Person;
 CREATE TABLE Person(
-    person_id integer NOT NULL PRIMARY KEY,
+    person_id BIGINT NOT NULL PRIMARY KEY,
     person_first_name varchar(255) NOT NULL,
     person_last_name varchar(255) NOT NULL,
     date_of_birth timestamp NOT NULL,
     street_name varchar(255) NOT NULL,
-    street_number integer NOT NULL,
-    apartment_number integer,
-    zipcode integer NOT NULL,
-    personal_number_id integer NOT NULL
+    street_number BIGINT NOT NULL,
+    apartment_number BIGINT,
+    zipcode BIGINT NOT NULL,
+    personal_number_id BIGINT NOT NULL
     );
 
 --Create Zipcode table--
 DROP TABLE Zipcode;
 CREATE TABLE Zipcode(
-    zipcode integer NOT NULL PRIMARY KEY,
+    zipcode BIGINT NOT NULL PRIMARY KEY,
     town varchar(255) NOT NULL
     );
 
 --Create Personal Number table--
 DROP TABLE PersonalNumber;
 CREATE TABLE PersonalNumber(
-    personal_number_id integer NOT NULL PRIMARY KEY
+    personal_number_id BIGINT NOT NULL PRIMARY KEY
     );
 
 --Create Parent table--
 DROP TABLE Parent;
 CREATE TABLE Parent(
-    parent_id integer NOT NULL,
-    child_id integer NOT NULL
+    parent_id BIGINT NOT NULL,
+    child_id BIGINT NOT NULL
     );
 
 --Create Spouse table--
 DROP TABLE Spouse;
 CREATE TABLE Spouse(
-    spouse_1_id integer NOT NULL,
-    spouse_2_id integer NOT NULL
+    spouse_1_id BIGINT NOT NULL,
+    spouse_2_id BIGINT NOT NULL
 );
 
 --Create Customer table--
 DROP TABLE Customer;
 CREATE TABLE Customer(
-    customer_id integer NOT NULL PRIMARY KEY,
+    customer_id BIGINT NOT NULL PRIMARY KEY,
     customer_type varchar(255) NOT NULL,
-    person_id integer NOT NULL
+    person_id BIGINT NOT NULL
 );
 
 --Create Customer Has Account table--
 DROP TABLE CustomerHasAccount;
 CREATE TABLE CustomerHasAccount(
-    customer_id integer NOT NULL,
-    account_id integer NOT NULL
+    customer_id BIGINT NOT NULL,
+    account_id BIGINT NOT NULL
 );
 
 --Create Employee table--
 DROP TABLE Employee;
 CREATE TABLE Employee(
-    employee_id integer NOT NULL PRIMARY KEY,
+    employee_id BIGINT NOT NULL PRIMARY KEY,
     employee_name varchar(255) NOT NULL,
-    employee_salary integer NOT NULL,
-    person_id integer NOT NULL
+    employee_salary BIGINT NOT NULL,
+    person_id BIGINT NOT NULL
 );
 
 --Create Employee Performs Cash Draft table--
 DROP TABLE EmployeePerformsCashDraft;
 CREATE TABLE EmployeePerformsCashDraft(
-    employee_id integer NOT NULL,
-    cash_draft_id integer NOT NULL
+    employee_id BIGINT NOT NULL,
+    cash_draft_id BIGINT NOT NULL
 );
 
 --Create Cash Draft table--
 DROP TABLE CashDraft;
 CREATE TABLE CashDraft(
-    cash_draft_id integer NOT NULL PRIMARY KEY,
+    cash_draft_id BIGINT NOT NULL PRIMARY KEY,
     transaction_date timestamp NOT NULL,
     transaction_amount real NOT NULL
 );
@@ -91,14 +91,14 @@ CREATE TABLE CashDraft(
 --Create Transaction Stored In Cash Draft table--
 DROP TABLE TransactionStoredInCashDraft;
 CREATE TABLE TransactionStoredInCashDraft(
-    cash_draft_id integer NOT NULL,
-    transaction_id integer NOT NULL
+    cash_draft_id BIGINT NOT NULL,
+    transaction_id BIGINT NOT NULL
 );
 
 --Create Transactions table--
 DROP TABLE Transactions;
 CREATE TABLE Transactions(
-    transaction_id SERIAL NOT NULL PRIMARY KEY AUTO,
+    transaction_id SERIAL NOT NULL PRIMARY KEY,
     transaction_type varchar(255) NOT NULL,
     transaction_time timestamp NOT NULL,
     transaction_amount real NOT NULL
@@ -107,7 +107,7 @@ CREATE TABLE Transactions(
 --Create Account table--
 DROP TABLE Account;
 CREATE TABLE Account(
-    account_id integer NOT NULL PRIMARY KEY,
+    account_id BIGINT NOT NULL PRIMARY KEY,
     account_type varchar(255) NOT NULL,
     balance real NOT NULL
 );
@@ -115,17 +115,22 @@ CREATE TABLE Account(
 --Create Account Performs Transaction table--
 DROP TABLE AccountPerformsTransaction;
 CREATE TABLE AccountPerformsTransaction(
-    account_id integer NOT NULL,
-    transaction_id integer NOT NULL
+    account_id BIGINT NOT NULL,
+    transaction_id BIGINT NOT NULL
 );
 
---Create Account Type  table--
+--Create Account Type table--
 DROP TABLE AccountType;
 CREATE TABLE AccountType(
-    account_type_id integer NOT NULL,
+    account_type_id BIGINT NOT NULL,
     interest_rate_name varchar(255) NOT NULL,
     interest_rate_value real NOT NULL,
-    account_id integer NOT NULL
+    account_id BIGINT NOT NULL
 );
 
---Create table--
+--Create Bank Has Account table--
+DROP TABLE BankHasAccount;
+CREATE TABLE BankHasAccount(
+    bank_id BIGINT NOT NULL,
+    account_id BIGINT NOT NULL
+);
