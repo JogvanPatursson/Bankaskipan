b944dffa693b81c2ee999755ee9c495e0b5cdf37
Jogvan Patursson <jogvan.pat18@student.setur.fo>
2021-06-10 15:47:58 +0100

    Update dml.sql

--- sql/dml.sql
+++ sql/dml.sql
@@ -215,23 +215,33 @@ $$
 
 
 --Get all transactions of customer--
-CREATE OR REPLACE FUNCTION getAllTransactions(account_id_variable varchar(255))
+CREATE OR REPLACE FUNCTION getAllTransactions(account_id_variable BIGINT)
     RETURNS TABLE(transaction_id_variable BIGINT, transaction_type_variable varchar(255), transaction_time_variable timestamp, transaction_amount_variable real)
     AS
     $$
+    DECLARE
+        transactions_record RECORD;
     BEGIN
-        SELECT *
-        FROM Transactions
-        WHERE transaction_id = (
-            SELECT transaction_id
-            FROM AccountPerformsTransaction
-            WHERE account_id = (
-                SELECT account_id
-                FROM Account
-                WHERE account_id = account_id_variable(
+        FOR transactions_record IN (
+            SELECT transaction_id, transaction_type, transaction_time, transaction_amount
+            FROM Transactions
+            WHERE transaction_id = (
+                SELECT transaction_id
+                FROM AccountPerformsTransaction
+                WHERE account_id = (
+                    SELECT account_id
+                    FROM Account
+                    WHERE account_id = account_id_variable
                 )
             )
-        );
+        ) LOOP
+            transaction_id_variable := transactions_record.transaction_id;
+            transaction_type_variable := transactions_record.transaction_type;
+            transaction_time_variable := transactions_record.transaction_time;
+            transaction_amount_variable := transactions_record.transaction_amount;
+
+            RETURN NEXT;
+        END LOOP;
     END;
     $$
     LANGUAGE 'plpgsql';
@@ -263,87 +273,29 @@ CREATE OR REPLACE FUNCTION showAllAccounts(customer_id_variable BIGINT)
     LANGUAGE 'plpgsql';
 
 --Show all spouses, and children of customer--
-CREATE OR REPLACE FUNCTION showAllSpousesOrChildren(customer_id_variable varchar(255))
-    RETURNS TABLE(child_or_spouse_id_variable BIGINT)
+CREATE OR REPLACE FUNCTION showAllRelatives(customer_id_variable BIGINT)
+    RETURNS TABLE(customer_1_id_variable BIGINT, person_first_name_variable VARCHAR(255), person_last_name_variable VARCHAR(255), customer_2_id_variable BIGINT)
     AS
     $$
+    DECLARE
+        relatives_record RECORD;
     BEGIN
-        SELECT p.*, c.customer_id FROM person p, customer c WHERE c.person_id = p.person_id AND p.person_id IN (
-            SELECT child_id
-            FROM Parent
-            WHERE parent_id = (
-                SELECT customer_id
-                FROM Customer
-                WHERE customer_id = customer_id_variable)
-            UNION
-            SELECT spouse_2_id
-            FROM Spouse
-            WHERE spouse_1_id = (
-                SELECT customer_id
-                FROM Customer
-                WHERE customer_id = customer_id_variable)
-            UNION
-            SELECT spouse_1_id
-            FROM Spouse
-            WHERE spouse_2_id = (
-                SELECT customer_id
-                FROM Customer
-                WHERE customer_id = customer_id_variable
-            )
-        );
+        FOR relatives_record IN (
+            SELECT customer_1_id, person_first_name, person_first_name, customer_2_id
+            FROM relatives
+            WHERE customer_1_id = customer_id_variable
+        ) LOOP
+            customer_1_id_variable := relatives_record.customer_1_id;
+            person_first_name_variable := relatives_record.person_first_name;
+            person_last_name_variable := relatives_record.person_first_name;
+            customer_2_id_variable := relatives_record.customer_2_id;
+
+            RETURN NEXT;
+        END LOOP;
     END;
     $$
     LANGUAGE 'plpgsql';
 
-DROP VIEW relatives;
-CREATE OR REPLACE VIEW relatives AS
-(SELECT c1.customer_id AS customer_1_id, p.person_first_name, p.person_last_name, c2.customer_id AS customer_2_id
-    FROM customer c1, person p, customer c2, spouse s
-    WHERE s.spouse_1_id = c1.customer_id
-        AND s.spouse_2_id = c2.customer_id
-        AND c2.person_id = p.person_id)
-UNION
-(SELECT c2.customer_id AS customer_2_id, p.person_first_name, p.person_last_name, c1.customer_id AS customer_1_id
-    FROM customer c1, person p, customer c2, spouse s
-    WHERE s.spouse_1_id = c1.customer_id
-        AND s.spouse_2_id = c2.customer_id
-        AND c1.person_id = p.person_id)
-UNION
-(SELECT c1.customer_id AS customer_1_id, p.person_first_name, p.person_last_name, c2.customer_id AS customer_2_id
-    FROM customer c1, person p, customer c2, parent pa
-    WHERE pa.parent_id = c1.customer_id
-        AND pa.child_id = c2.customer_id
-        AND c2.person_id = p.person_id);
-
-
-    
-
---Show all accounts of child--    
-CREATE OR REPLACE FUNCTION showAllAccountsOfChild(child_id_variable varchar(255))
-    RETURNS TABLE(account_id_variable BIGINT)
-    AS
-    $$
-    BEGIN
-        SELECT account_id
-        FROM CustomerHasAccount
-        WHERE customer_id = child_id_variable;
-    END;
-    $$
-    LANGUAGE 'plpgsql';
-
-
---Show all accounts of spouse--
-CREATE OR REPLACE FUNCTION showAllAccountsOfSpouse(spouse_2_id_variable varchar(255))
-    RETURNS TABLE(account_id_variable BIGINT)
-    AS
-    $$
-    BEGIN
-        SELECT account_id
-        FROM CustomerHasAccount
-        WHERE customer_id = spouse_2_id_variable;
-    END;
-    $$
-    LANGUAGE 'plpgsql';
 
 
 ----------------------------------
@@ -415,7 +367,7 @@ CREATE OR REPLACE PROCEDURE insertIntoAccountPerformsTransaction(account_id_vari
     LANGUAGE 'plpgsql';
 
 --AccountType--
-CREATE OR REPLACE PROCEDURE insertIntoAccountType(account_type_id_variable BIGINT, interest_rate_name_variable varchar(255), interest_rate_value_variable real, account_id_variable BIGINT)
+CREATE OR REPLACE PROCEDURE insertIntoAccountType(interest_rate_name_variable varchar(255), interest_rate_value_variable real, account_id_variable BIGINT)
     AS
     $$
     BEGIN
@@ -424,7 +376,7 @@ CREATE OR REPLACE PROCEDURE insertIntoAccountType(account_type_id_variable BIGIN
         interest_rate_name,
         interest_rate_value,
         account_id)
-    Values(account_type_id_variable, interest_rate_name_variable, interest_rate_value_variable, account_id_variable);
+    Values(DEFAULT, interest_rate_name_variable, interest_rate_value_variable, account_id_variable);
     COMMIT;
     END;
     $$
@@ -432,7 +384,7 @@ CREATE OR REPLACE PROCEDURE insertIntoAccountType(account_type_id_variable BIGIN
 
 
 --CashDraft--
-CREATE OR REPLACE PROCEDURE insertIntoCashDraft(cash_draft_id_variable BIGINT, transaction_date_variable timestamp, transaction_amount_variable real)
+CREATE OR REPLACE PROCEDURE insertIntoCashDraft(transaction_date_variable timestamp, transaction_amount_variable real)
     AS
     $$
     BEGIN
@@ -440,7 +392,7 @@ CREATE OR REPLACE PROCEDURE insertIntoCashDraft(cash_draft_id_variable BIGINT, t
         cash_draft_id,
         transaction_date,
         transaction_amount)
-    Values(cash_draft_id_variable, transaction_date_variable, transaction_amount_variable);
+    Values(DEFAULT, transaction_date_variable, transaction_amount_variable);
     COMMIT;
     END;
     $$
@@ -448,7 +400,7 @@ CREATE OR REPLACE PROCEDURE insertIntoCashDraft(cash_draft_id_variable BIGINT, t
 
 
 --Customer--
-CREATE OR REPLACE PROCEDURE insertIntoCustomer(customer_id_variable BIGINT, customer_type_variable varchar(255), person_id_variable BIGINT)
+CREATE OR REPLACE PROCEDURE insertIntoCustomer(customer_type_variable varchar(255), person_id_variable BIGINT)
     AS
     $$
     BEGIN
@@ -456,7 +408,7 @@ CREATE OR REPLACE PROCEDURE insertIntoCustomer(customer_id_variable BIGINT, cust
         customer_id,
         customer_type,
         person_id)
-    Values(customer_id_variable, customer_type_variable, person_id_variable);
+    Values(DEFAULT, customer_type_variable, person_id_variable);
     COMMIT;
     END;
     $$
@@ -479,7 +431,7 @@ CREATE OR REPLACE PROCEDURE insertIntoCustomerHasAccount(customer_id_variable BI
 
 
 --Employee--
-CREATE OR REPLACE PROCEDURE insertIntoEmployee(employee_id_variable BIGINT, employee_name_variable varchar(255), employee_salary_variable real, person_id_variable BIGINT)
+CREATE OR REPLACE PROCEDURE insertIntoEmployee(employee_name_variable varchar(255), employee_salary_variable real, person_id_variable BIGINT)
     AS
     $$
     BEGIN
@@ -488,7 +440,7 @@ CREATE OR REPLACE PROCEDURE insertIntoEmployee(employee_id_variable BIGINT, empl
         employee_name,
         employee_salary,
         person_id)
-    Values(employee_id_variable, employee_name_variable, employee_salary_variable, person_id_variable);
+    Values(DEFAULT, employee_name_variable, employee_salary_variable, person_id_variable);
     COMMIT;
     END;
     $$
@@ -526,7 +478,7 @@ CREATE OR REPLACE PROCEDURE insertIntoParent(parent_id_variable BIGINT, child_id
 
 
 --Person--
-CREATE OR REPLACE PROCEDURE insertIntoPerson(person_id_variable BIGINT, person_first_name_variable varchar(255), person_last_name_variable varchar(255), date_of_birth_variable timestamp, street_name_variable varchar(255), street_number_variable BIGINT, apartment_number_variable BIGINT, zipcode_variable BIGINT, person_number_id_variable BIGINT)
+CREATE OR REPLACE PROCEDURE insertIntoPerson(person_first_name_variable varchar(255), person_last_name_variable varchar(255), date_of_birth_variable timestamp, street_name_variable varchar(255), street_number_variable BIGINT, apartment_number_variable BIGINT, zipcode_variable BIGINT, person_number_id_variable BIGINT)
     AS
     $$
     BEGIN
@@ -540,7 +492,7 @@ CREATE OR REPLACE PROCEDURE insertIntoPerson(person_id_variable BIGINT, person_f
         apartment_number,
         zipcode,
         personal_number_id)
-    VALUES (person_id_variable, person_first_name_variable, person_last_name_variable, date_of_birth_variable, street_name_variable, street_number_variable, apartment_number_variable, zipcode_variable, person_number_id_variable);
+    VALUES (DEFAULT, person_first_name_variable, person_last_name_variable, date_of_birth_variable, street_name_variable, street_number_variable, apartment_number_variable, zipcode_variable, person_number_id_variable);
     COMMIT;
     END;
     $$
@@ -577,7 +529,7 @@ CREATE OR REPLACE PROCEDURE insertIntoSpouse(spouse_1_id_variable BIGINT, spouse
 
 
 --Transactions--
-CREATE OR REPLACE PROCEDURE insertIntoTransactions(transaction_id_variable BIGINT, transaction_type_variable varchar(255), transaction_time_variable timestamp,transaction_amount_variable real)
+CREATE OR REPLACE PROCEDURE insertIntoTransactions(transaction_type_variable varchar(255), transaction_time_variable timestamp,transaction_amount_variable real)
     AS
     $$
     BEGIN
@@ -586,7 +538,7 @@ CREATE OR REPLACE PROCEDURE insertIntoTransactions(transaction_id_variable BIGIN
         transaction_type,
         transaction_time,
         transaction_amount)
-    Values(transaction_id_variable, transaction_type_variable, transaction_time_variable, transaction_amount_variable);
+    Values(DEFAULT, transaction_type_variable, transaction_time_variable, transaction_amount_variable);
     COMMIT;
     END;
     $$
